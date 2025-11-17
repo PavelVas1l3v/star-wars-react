befb4ff0a99f6c3e06070eab267ca3a58aa3ebc4
Pavel Vasilev <paul25072000@gmail.com>
2025-11-17 13:32:09 +0200

    Second commit: added navigation bar on react added Header, Footer, Main

---
+++ src/components/NavItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const NavItem = ({itemTitle}) => {
+    return (
+        <li className="nav-item btn btn-danger mx-1 border-warning">
+            {itemTitle}
+        </li>
+    );
+};
+
+export default NavItem;
