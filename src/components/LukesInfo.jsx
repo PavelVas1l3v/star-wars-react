d935ae9b2e598ec3a0ec2bc2f0efda4bf8c6fc34
Pavel Vasilev <paul25072000@gmail.com>
2025-11-25 18:51:03 +0200

    Commit before homework

--- src/components/LukesInfo.jsx
+++ src/components/LukesInfo.jsx
@@ -1,9 +1,30 @@
-import {textGalaxy} from "../utils/constants.js";
+// import {openingCrawl} from "../utils/constants.js";
+import {useEffect, useState} from "react";
+import {base_url} from "../utils/constants.js";
+
 
 const LukesInfo = () => {
-    return (
-        <p className="far-galaxy">{textGalaxy}</p>
+    const [openingCrawl, setOpeningCrawl] = useState();
+
+    useEffect(() => {
+        const episode= Math.floor(Math.random() * 6)+1
+        fetch(`${base_url}/v1/films/${episode}`)
+            .then(res => res.json())
+            .then(data => setOpeningCrawl(data.opening_crawl))
+            .catch(() => setOpeningCrawl('Failed to fetch data.',));
+    },[])
+    if(openingCrawl)
+        return (
+        <p className="far-galaxy">{openingCrawl}</p>
     );
+    else {
+        return (
+            <p className="far-galaxy">
+                <span className={'spinner-border-sm spinner-border'}></span>
+                <span className={'spinner-grow spinner-grow-sm'}>Loading...</span>
+            </p>
+            );
+    }
 };
 
 export default LukesInfo;
