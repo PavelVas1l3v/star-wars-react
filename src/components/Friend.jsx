5f3f1bd3f0060a83df4ebdf6de325e2edc9e14bd
Pavel Vasilev <paul25072000@gmail.com>
2025-12-05 15:08:26 +0200

    First commit

---
+++ src/components/Friend.jsx
@@ -0,0 +1,14 @@
+const Friend = ({friend, pos}) => {
+    let styles = "w-full ";
+    if (pos === 7) {
+        styles += 'rounded-bl-3xl';
+    }
+    if (pos === 9) {
+        styles += 'rounded-br-3xl';
+    }
+    return (
+        <img className={styles} src={friend} alt="Friend"/>
+    )
+}
+
+export default Friend;
